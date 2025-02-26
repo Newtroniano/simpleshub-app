e254a1e6e5308eb3644dbbffc253dbf8266b6860
unknown <luisocara.luis@gmail.com>
2025-02-26 02:24:04 -0300

    docker

--- backend/scr/utils/cpfValidator.js
+++
@@ -1,8 +0,0 @@
-class CpfValidator {
-    static validateFormat(cpf) {
-      const regex = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
-      return regex.test(cpf);
-    }
-  }
-  
-  module.exports = CpfValidator;
